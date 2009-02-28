b42b5531d4c9c7ddf80910bd7bbf23245909eb25
willkurt <wckurt@gmail.com>
2009-02-28 13:58:44 -0800

    added some important mehtods to the header

--- SymbolTable.hpp
+++ SymbolTable.hpp
@@ -11,9 +11,20 @@
 #include "symbol_content.hpp"
 
 class SymbolTable{
-  std::stack<std::map<std::string,SymbolContent> > st;
+  std::stack<std::map<std:: string, SymbolContent> > st;
 public:
   SymbolTable();
+
+  void Add(std::string,SymbolContent){};
+
+  SymbolContent* SearchTop(std::string){};
+
+  SymbolContent* SearchAll(std::string){};
+  
+  bool Shadowing(std::string){};
+
+  //need to implement Dump() but not sure exactly wat we ant
+
 };
 
 #endif
