68a5faf3ce13db97135bcde1c36154dd0564e9cf
willkurt <wckurt@gmail.com>
2009-03-24 00:04:20 -0700

    back to a place I'm happy wiht

--- y.tab.h
+++ y.tab.h
@@ -172,17 +172,44 @@
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
-#line 14 "new_c_grammar.y"
+#line 16 "test_c_grammar.y"
 {
+  //consider making this a string pointer
   char* sval;
   float dval;
   int ival;
   char cval;
-  /*right now I don't know how to put structs in, so we'll fake it*/
-  SymbolContent* scptrval;
+  /* barrayval is for all the possible specifies
+     here is the key:
+     * --storage
+     * 0-AUTO
+     * 1-REGISTER
+     * 2-STATIC
+     * 3-EXTERN
+     * 4-TYPEDEF
+     * --Types
+     * 5-VOID
+     * 6-CHAR
+     * 7-SHORT
+     * 8-INT
+     * 9-LONG
+     *10-FLOAT
+     *11-DOUBLE
+     *12-SIGNED
+     *13-UNSIGNED
+     *14-STRUCT
+     *15-UNION
+     *16-ENUM no idea what I'm doing with this!
+     *17-TYPEDEF_NAME
+     *---type qualifiers
+     *18-CONST
+     *19-VOLATILE
+   */
+  bool barrayval[20];
+
  }
 /* Line 1489 of yacc.c.  */
-#line 186 "y.tab.h"
+#line 213 "y.tab.h"
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
